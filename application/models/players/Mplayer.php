78ffb0db4e7ab5e8d6326f98cd3f5528e45c0ad4
RRangika <ruchinide.silva077@gmail.com>
2018-06-17 11:12:59 +0530

    Added Models and Data(MPlayer)and removed Pusher codeigniter file

---
+++ application/models/players/Mplayer.php
@@ -0,0 +1,8 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+class Mplayer extends CI_Model
+{
+    function __construct (){
+            parent::__construct();
+    }
+}
+
