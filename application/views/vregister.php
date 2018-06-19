eb9a25db0f579321e2f9edeb33b30b4aab2a0a68
Saku-SP <spabasari@gmail.com>
2018-06-19 18:07:39 +0530

    Changed the Register view

--- application/views/vregister.php
+++ application/views/vregister.php
@@ -18,88 +18,73 @@
                     <h2>User Registration</h2>
                 </div>
                 <div class="panel-body">
-
-                    <select name="user_type_select" id="user_type_select" class="form-control select-box"
-                            data-placeholder="Select user type"
-                            title="User Type">
-                        <option></option>
+                    Select the User type
+                    <select name="user_type_select" id="user_type_select" class="form-control select-box" data-placeholder="Select user type" title="User Type">
+                        <option value="" selected disabled>Select the User Type</option>
                         <option value="player">Player</option>
                         <option value="broker">Broker</option>
                     </select>
 
-                    <form action="<?php echo base_url("register/save"); ?>" class="form-horizontal validation"
-                          id="broker-form" style="display: none;"
-                          method="post">
-                        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>"
-                               value="<?php echo $this->security->get_csrf_hash(); ?>">
+                    <form action="<?php echo base_url("register/save"); ?>" class="form-horizontal validation" id="broker-form" style="display: none;" method="post">
+                        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
 
-                        <input type="hidden" name="user_type" id="user_type_broker">
+                        <input type="hidden" name="user_type" id="user_type_broker" value="brokers">
 
                         <div class="form-group">
-                            <select name="company" id="company" class="form-control select-box"
-                                    data-placeholder="Select Company" title="Select Company" required>
-                                <option></option>
-                                <option value="1">company 1</option>
+                            Select the Company
+                            <select name="company" id="company" class="form-control select-box" data-placeholder="Select Company" title="Select Company" required>
+                                <option value="" selected disabled>Select the Company</option>
+                                <?php
+                                if (isset($companies)) {
+                                    foreach ($companies as $row) {
+                                        echo '<option value="' . $row['user_id'] . '">' . $row['company_name'] . '</option>';
+                                    }
+                                }
+                                ?>
                             </select>
                         </div>
 
                         <div class="form-group">
-                            <input type="text" name="name" id="name" class="form-control" title="Name"
-                                   placeholder="Enter your name" required>
+                            <input type="text" name="name" id="name" class="form-control" title="Name" placeholder="Enter your name" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="email" name="email" id="email" class="form-control" title="Email"
-                                   placeholder="Enter your email" required>
+                            <input type="email" name="email" id="email" class="form-control" title="Email" placeholder="Enter your email" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="password" title="Password" name="password" id="password"
-                                   class="form-control" placeholder="Enter your password" required>
+                            <input type="password" title="Password" name="password" id="password" class="form-control" placeholder="Enter your password" required>
                         </div>
                         <div class="form-group">
-                            <input type="password" title="Password" name="password_confirm" id="password_confirm"
-                                   class="form-control" placeholder="Confirm your password"
-                                   data-parsley-equalto="#password"
-                                   required>
+                            <input type="password" title="Password" name="password_confirm" id="password_confirm" class="form-control" placeholder="Confirm your password" data-parsley-equalto="#password" required>
                         </div>
 
                         <input type="submit" class="btn btn-primary btn-raised pull-right" value="Register"/>
 
                     </form>
 
-                    <form action="<?php echo base_url("register/save"); ?>" class="form-horizontal validation"
-                          id="player-form" style="display: none;"
-                          method="post">
-                        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>"
-                               value="<?php echo $this->security->get_csrf_hash(); ?>">
+                    <form action="<?php echo base_url("register/save"); ?>" class="form-horizontal validation" id="player-form" style="display: none;" method="post">
+                        <input type="hidden" name="<?php echo $this->security->get_csrf_token_name(); ?>" value="<?php echo $this->security->get_csrf_hash(); ?>">
 
-                        <input type="hidden" name="user_type" id="user_type_player">
+                        <input type="hidden" name="user_type" id="user_type_player" value="players">
+                        <input type="hidden" name="player_type" id="player_type_player" value="human">
 
                         <div class="form-group">
-                            <input type="text" name="name" id="name" class="form-control" title="Name"
-                                   placeholder="Enter your name" required>
+                            <input type="text" name="name" id="name" class="form-control" title="Name" placeholder="Enter your name" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="email" name="email" id="email" class="form-control" title="Email"
-                                   placeholder="Enter your email" required>
+                            <input type="email" name="email" id="email" class="form-control" title="Email" placeholder="Enter your email" required>
                         </div>
 
                         <div class="form-group">
-                            <input type="password" title="Password" name="password" id="p-password"
-                                   class="form-control" placeholder="Enter your password" required>
+                            <input type="password" title="Password" name="password" id="p-password" class="form-control" placeholder="Enter your password" required>
                         </div>
                         <div class="form-group">
-                            <input type="password" title="Password" name="password_confirm" id="password_confirm"
-                                   class="form-control" placeholder="Confirm your password"
-                                   data-parsley-equalto="#p-password"
-                                   required>
+                            <input type="password" title="Password" name="password_confirm" id="password_confirm" class="form-control" placeholder="Confirm your password" data-parsley-equalto="#p-password" required>
                         </div>
                         <div class="form-group">
-                            <input type="number" name="initial_balance" id="initial_balance"
-                                   placeholder="Enter your initial balance" title="Initial Balance"
-                                   class="form-control">
+                            <input type="number" name="initial_balance" id="initial_balance" placeholder="Enter your initial balance" title="Initial Balance" class="form-control">
                         </div>
 
                         <input type="submit" class="btn btn-primary btn-raised pull-right" value="Register"/>
@@ -108,8 +93,7 @@
                 </div>
                 <div class="panel-footer">
                     <hr/>
-                    <a href="<?php echo base_url('/login'); ?>"
-                       class="btn btn-primary btn-raised btn-block">
+                    <a href="<?php echo base_url('/login'); ?>" class="btn btn-primary btn-raised btn-block">
                         Login
                     </a>
                 </div>
@@ -122,7 +106,6 @@
     $(document).ready(function () {
         $('#user_type_select').on('change', function () {
             const value = this.value;
-            $('#user_type_' + value).val(value);
             if ($(`form`).is(':visible')) {
                 $('form').slideUp('slow', function () {
                     $('#' + value + '-form').slideDown('slow');
