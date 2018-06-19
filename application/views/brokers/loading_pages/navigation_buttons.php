e7c0a0a2c7c5ec8f640c7ec2bc98c125dd532884
chamikamanathunga <chamikamanathunga98@gmail.com>
2018-06-19 18:44:49 +0530

    Added Broker Views 1 - Navigation Buttons

---
+++ application/views/brokers/loading_pages/navigation_buttons.php
@@ -0,0 +1,11 @@
+<a class="btn btn-warning btn-lg" style="width: 100%"
+   href="<?php echo base_url() . $this->session->userdata('user_type') . '/view-portfolio'; ?>">View Portfolio of
+    Player</a>
+<a class="btn btn-warning btn-lg" style="width: 100%"
+   href="<?php echo base_url() . $this->session->userdata('user_type') . '/players-transactions'; ?>">Player's
+    Transactions</a>
+<a class="btn btn-warning btn-lg" style="width: 100%"
+   href="<?php echo base_url() . $this->session->userdata('user_type') . '/stock-management'; ?>">Stock Management</a>
+<a class="btn btn-warning btn-lg" style="width: 100%"
+   href="<?php echo base_url() . $this->session->userdata('user_type') . '/view-price-of-stock'; ?>">View
+    Price of Stock</a>
