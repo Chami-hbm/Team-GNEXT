b1c05e83b38547887f06eff30d4fa780ad1471ca
RRangika <ruchinide.silva077@gmail.com>
2018-06-19 15:57:42 +0530

    Register save functionality changed a little bit with required things

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,9 +7,10 @@ class Home extends CI_controller
     function __construct() {
         parent::__construct();
         $this->load->model('mlogin');
+        $this->load->model('mhome');
         $this->data = array(
             'site_name' => $this->config->item('site_name'),
-            'title' => $this->config->item('site_name') . ' - Login',
+            'title' => $this->config->item('site_name'),
         );
     }
     
@@ -35,7 +36,6 @@ class Home extends CI_controller
             } else {
                 redirect(base_url() . $this->session->userdata('user_type') . '/view-portfolio');
             }
-//             echo $this->session->userdata('usertype');
         } else {
             redirect(base_url() . 'login');
         }
@@ -46,29 +46,29 @@ class Home extends CI_controller
     public function register() {
         $data = $this->data;
         $data['title'] = $this->config->item('site_name') . ' - Register';
+        $data['companies'] = $this->mhome->get_companies();
         
         $this->load->view('includes/v_include_header', $data);
         $this->load->view('vregister');
         $this->load->view('includes/v_include_footer');
     }
     
-    /**
-     *
-     */
     public function register_save() {
         $user_data['user_type'] = $this->input->post('user_type');
         $user_data['name'] = $this->input->post('name');
         $user_data['email'] = $this->input->post('email');
-        $user_data['password'] = $this->input->post('password');
+        $user_data['password'] = hash('sha512', $this->input->post('password'));
+        $user_data['password_not_hashed'] = $this->input->post('password');
         
-        if ($user_data['user_type'] === 'player') {
-            $user_data['initial_balance'] = $this->input->post('initial_balance');
-        } elseif ($user_data['user_type'] === 'broker') {
-            $user_data['company_id'] = $this->input->post('company');
+        if ($user_data['user_type'] === 'players') {
+            $user_data['player_type'] = $this->input->post('player_type');
+            $user_data['current_balance'] = $this->input->post('initial_balance');
+        } elseif ($user_data['user_type'] === 'brokers') {
+            $user_data['user_id'] = $this->input->post('company');
         }
     
         $this->mlogin->register($user_data);
     
-//        redirect(base_url('login'));
+        redirect(base_url('login'));
     }
 }
