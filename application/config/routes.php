2bcac3edecda3e99dafc9ec589faeb52c57f02da
s-samarajeewa <ssamadhisudeshini@gmail.com>
2018-06-21 19:08:53 +0530

    Clock functionality and Stock Changing Algorithm completed in controllers

--- application/config/routes.php
+++ application/config/routes.php
@@ -9,32 +9,18 @@ $route['404_override'] = 'home/error404';
 $route['login'] = "home/login";
 $route['login-check'] = "home/login_check";
 $route['register'] = "home/register";
-$route['register/save'] = "home/register_save";
 $route['logout'] = "home/logout";
 
 //Players
-$route['players/play-game'] = 'players/players_play_game/index';
-$route['players/get-analyst'] = 'players/players/get_analyst';
-$route['players/view-bank-balance'] = 'players/players/view_bank_balance';
-$route['players/view-stock-portfolio'] = 'players/players_portfolio/index';
-$route['players/view-current-historical-price-shares'] = 'players/players_historical_price/index';
-
-//Players Stocks
-$route['players/stocks/buy/save'] = 'players/players_stock/save_buy';
+$route['players/play-game'] = 'players/play_game';
+$route['players/get-analyst'] = 'players/get_analyst';
+$route['players/view-bank-balance'] = 'players/view-bank-balance';
+$route['players/view-stock-portfolio'] = 'players/view-stock-portfolio';
+$route['players/view-current-historical-price-shares'] = 'players/view-current-historical-price-shares';
 
 
 //Brokers
-$route['brokers/view-portfolio'] = 'brokers/brokers/view_portfolio';
-$route['brokers/players-transactions'] = 'brokers/brokers_transactions/index';
-$route['brokers/stock-management'] = 'brokers/brokers_stock/index';
-$route['brokers/view-price-of-stock'] = 'brokers/brokers_historical_price/index';
-
-
-
-
-
-//Clock
-$route['clock/change-prices-per-turn'] = "clock/change_prices_per_turn";
+$route['brokers/view-portfolio'] = 'brokers/view_portfolio';
 
 
 
