efdf767835cbab0323efcb09f0b165c64515893e
chami-hbm <hbmchamikara@gmail.com>
2018-06-25 16:47:04 +0530

    Analyst Recommendation functionality finished in models not tested

--- application/models/M_company_stock.php
+++ application/models/M_company_stock.php
@@ -7,6 +7,7 @@ class M_company_stock extends CI_Model {
 
     function __construct() {
         parent::__construct();
+        $this->load->model('m_player_stock');
     }
 
     function get_stocks() {
@@ -19,7 +20,7 @@ class M_company_stock extends CI_Model {
         $result = $query->result_array();
         return $result;
     }
-
+    
     function decrease_stock_qty($stock_id, $qty) {
         $this->db->where('company_stock_id', $stock_id);
         $this->db->set('quantity', 'quantity-' . $qty, FALSE);
@@ -50,5 +51,45 @@ class M_company_stock extends CI_Model {
         return $this->db->get()->result_array();
         
     }
+        
+    function get_stocks_for_buy_recommendation() {
+//        $this->db->select('*,CASE WHEN status = "Approved" THEN "approved statement" 
+//                         WHEN status = "Disapproved" THEN "disapproved statement"
+//                                                     ELSE "pending statement"
+//                                                     END AS status');
+        $this->db->select('*');
+        $this->db->from('company_stocks as cs');
+        $this->db->join('users as cp', 'cs.users_user_id = cp.user_id', 'left');
+        $this->db->where('(((price - previous_price)/previous_price)*100)>=', '10');
+        $this->db->where('type', 'sell');
+        $this->db->where('cp.company_name !=',NULL);
+        $query = $this->db->get();
+        $result = $query->result_array();
+        return $result;
+    }
+    
+    function get_stocks_for_sell_recommendation() {
+        $this->db->select('*,cs.previous_price as cs_previous_price,cs.price as cs_price,br.company_name as br_company_name,br.company_sector as br_company_sector,ps.quantity as ps_quantity');
+        $this->db->from('company_stocks as cs');
+        $this->db->join('users as br', 'cs.users_user_id = br.user_id');
+        $this->db->join('player_stocks as ps', 'ps.company_stocks_company_stock_id = cs.company_stock_id');
+        $this->db->join('users as p', 'ps.users_user_id = p.user_id');
+        $this->db->where('(((cs.previous_price - cs.price)/cs.previous_price)*100)>=', '10');
+        $this->db->where('cs.type', 'sell');
+        $this->db->where('br.company_name !=',NULL);
+        $query = $this->db->get();
+        $result = $query->result_array();
+        
+//        $sell_set=array();
+//        $i=0;
+//        $this->m_player_stock->get_stock_for_sell()
+//        foreach ($result as $value) {
+//            
+//            
+//        }
+        
+        return $result;
+    }
+
 
 }
